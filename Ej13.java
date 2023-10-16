e79d08fbaf2d835ce91b38c6c280c0ac96976d55
Jalto28 <58223109+Jalto28@users.noreply.github.com>
2023-10-16 18:50:53 +0200

    Update Ej13.java

--- Ej13.java
+++ Ej13.java
@@ -28,8 +28,6 @@ public class Ej13 {
 
         // Mostrar resultados
         System.out.println("Porcentaje de caras: " + porcentajeCaras + "%");
-        System.out.println("Porcentaje de cruces: " + porcentajeCruces + "%");
-
-        teclado.close();
+        System.out.println("Porcentaje de cruces: " + porcentajeCruces + "%");        
     }
 }
